be17d1426953e99ca238612a41d8482bfdfe2aa8
MiguelOttoDev <105008843+MiguelOttoDev@users.noreply.github.com>
2024-12-01 00:22:52 -0300

    implementando os card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
+import ArticleList from './components/ArticlesList/ArticleList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Header/>
+     <ArticleList/>
     </>
   )
 }
